b0f90d9eacc8a550f103e43cbc99475fabdfc642
Thiago Caiubi <thiago.cruz@neoway.com.br>
2018-01-17 15:00:04 -0200

    Make random configuration changes

--- terraform/providers/azure/stg/main.tf
+++ terraform/providers/azure/stg/main.tf
@@ -14,3 +14,25 @@ module "network" {
 
   address_space = "${var.address_space}"
 }
+
+module "cargo" {
+  source = "../../../modules/cargo"
+
+  env      = "${var.env}"
+  location = "${var.location}"
+
+  platform_fault_domain_count         = "${var.cargo_platform_fault_domain_count}"
+  platform_update_domain_count        = "${var.cargo_platform_update_domain_count}"
+  trusted_ip                          = "${var.trusted_ip}"
+  subnet_address_prefix               = "${var.cargo_subnet_address_prefix}"
+  virtual_machine_admin_password      = "${var.cargo_virtual_machine_admin_password}"
+  virtual_machine_admin_username      = "${var.cargo_virtual_machine_admin_username}"
+  virtual_machine_count               = "${var.cargo_virtual_machine_count}"
+  virtual_machine_size                = "${var.cargo_virtual_machine_size}"
+  virtual_network_name                = "${module.network.name}"
+  virtual_network_resource_group_name = "${module.network.resource_group_name}"
+}
+
+output "cargo_load_balancer_public_ip_address" {
+  value = "${module.cargo.load_balancer_public_ip_address}"
+}
--- terraform/providers/azure/stg/terraform.tfvars
+++ terraform/providers/azure/stg/terraform.tfvars
@@ -1,5 +1,5 @@
 env = "stg"
-location = "eastus"
+location = "eastus2"
 
 address_space = ["10.20.0.0/16"]
 cargo_subnet_address_prefix = "10.20.1.0/24"
@@ -8,5 +8,7 @@ cargo_platform_fault_domain_count = 3
 cargo_platform_update_domain_count = 5
 cargo_virtual_machine_admin_password = ""
 cargo_virtual_machine_admin_username = "core"
-cargo_virtual_machine_count = 1
-cargo_virtual_machine_size = "Standard_A1"
+cargo_virtual_machine_count = 5
+cargo_virtual_machine_size = "Standard_DS2_v2"
+
+trusted_ip = "0.0.0.0/0"
